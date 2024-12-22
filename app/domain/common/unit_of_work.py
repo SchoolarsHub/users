85eabb6aaf9bf20819597be11a9ce6cf9ee29153
NxtaSunstrike <unixalle765@mail.ru>
2024-12-22 05:32:56 -0500

    (remove) abstractmethod from unit_of_work tracker in domain

--- app/domain/common/unit_of_work.py
+++ app/domain/common/unit_of_work.py
@@ -1,16 +1,12 @@
-from abc import abstractmethod
 from typing import Protocol
 
 
 class UnitOfWorkTracker[EntityT](Protocol):
-    @abstractmethod
     def register_new(self, entity: EntityT) -> None:
         raise NotImplementedError
 
-    @abstractmethod
     def register_dirty(self, entity: EntityT) -> None:
         raise NotImplementedError
 
-    @abstractmethod
     def register_deleted(self, entity: EntityT) -> None:
         raise NotImplementedError
