03198d131766488dc37ca4fe82f7f5b5f358f13f
NxtaSunstrike <unixalle765@mail.ru>
2024-12-22 05:09:36 -0500

    (add) uowed entity to domain in common folder

---
+++ app/domain/common/uowed_entity.py
@@ -0,0 +1,16 @@
+from app.domain.common.unit_of_work import UnitOfWorkTracker
+
+
+class UowedEntity[EntityId]:
+    def __init__(self, entity_id: EntityId, unit_of_work: UnitOfWorkTracker) -> None:
+        self.entity_id = entity_id
+        self.unit_of_work = unit_of_work
+
+    def mark_new(self) -> None:
+        self.unit_of_work.register_new(self)
+
+    def mark_dirty(self) -> None:
+        self.unit_of_work.register_dirty(self)
+
+    def mark_deleted(self) -> None:
+        self.unit_of_work.register_deleted(self)
