365644e91f4d89fc156caf5002d7e89ec0a6ecf1
NxtaSunstrike <unixalle765@mail.ru>
2024-12-30 16:27:37 -0500

    (remove) all

--- app/presentation/api/middlewares/cors_middleware.py
+++ app/presentation/api/middlewares/cors_middleware.py
@@ -7,6 +7,6 @@ def setup_cors_middleware(app: FastAPI) -> None:
         CORSMiddleware,
         allow_origins=["http://localhost:3000", "http//127.0.0.1:3000"],
         allow_credentials=True,
-        allow_methods=["POST", "GET", "PUT", "DELETE"],
+        allow_methods=["POST", "GET", "PUT"],
         allow_headers=["*"],
     )
