d26bdd4700abef7beea3dde62048484e976f70a2
NxtaSunstrike <unixalle765@mail.ru>
2025-01-06 13:07:03 -0500

    (add) configurations

--- app/infrastructure/config.py
+++ app/infrastructure/config.py
@@ -1,10 +1,24 @@
 from dataclasses import dataclass, field
+from tomllib import load
+
+from adaptix import Retort
 
 from app.infrastructure.brokers.rabbit.config import RabbitConfig
 from app.infrastructure.databases.postgres.config import PostgresConfig
+
+CONFIG_PATH = "./conf/app_config.toml"
 
 
 @dataclass(frozen=True)
 class Config:
     rabbit: RabbitConfig = field(default_factory=RabbitConfig)
     postgres: PostgresConfig = field(default_factory=PostgresConfig)
+
+    @staticmethod
+    def load_variables() -> "Config":
+        with open(CONFIG_PATH, "rb") as f:
+            config = load(f)
+
+        retort = Retort()
+
+        return retort.load(config, Config)
